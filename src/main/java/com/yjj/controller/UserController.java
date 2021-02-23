08596a0ae7a1b85f68df92767e80c70b309dba2b
yjj <2541557955@qq.com>
2021-02-23 18:38:02 +0800

    用户

--- src/main/java/com/yjj/controller/UserController.java
+++ src/main/java/com/yjj/controller/UserController.java
@@ -10,20 +10,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 用户
  * @author
  * @date: 2021/2/22 10:55
  */
